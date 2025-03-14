d151b6208d24009f0c09513f9b27886a029bec66
Luca <lfcaruccio@gmail.com>
2025-03-14 14:37:53 -0700

    HTML layout established, pending functionality assessment

--- books.js
+++ books.js
@@ -1,1 +1,20 @@
-const auhtors = await fetch (`https://openlibrary.org/search.json?author=${}`)git push -u origin main
+const authorListElements = document.querySelector(`.author__list`);
+const author = localStorage.getItem("author");
+
+async function onSearchChange(event) {
+    const author = event.target.value
+    renderAuth(title);
+}
+
+async function renderAuth(title) {
+    const auth = await fetch(`https://openlibrary.org/search.json?author=${author}`)
+    const authData = await auth.json();
+    authortListElements.innerHTML = authData.map(auth => authHTML(auth)).join('');
+}
+
+function authHTML(auth) {
+    return
+    //not sure what to put here
+}
+
+renderAuth(title);
--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-const
+const bookListElements = document.querySelector(`.book-list`);
 
 async function main() {
     const books = await fetch("https://openlibrary.org/search.json")
+    const authors = await fetch("https://openlibrary.org/search.json?author=${author}")
     const booksData = await books.json();
+        bookListElements = booksData
+        .map((book) => bookHTML(book)).join("");
     
 }
 
 main();
+
+function showBooksTitle(book) {
+    localStorage.setItem("")
+    window.location.href = `${window.location.origin}/book.html`
+}
+
+function showBooksAuthor(author) {
+     localStorage.setItem("")
+    window.location.href = `${window.location.origin}/author.html`
+}
+
+function bookHTML(book) {
+    return `<div class="book__cover" onclick="showBooksAuthor(${author})">
+                <div class="book__cover--container">
+                  <h3>${book.title}</h4>
+                    <p><b>Book Title:</b>${title}</p>
+                    <p><b>Author:</b> ${author.name}</p>
+                    <p><b>Link:</b> <a href="${book.website}" target="_blank">${book.website}</a></p>
+                </div>
+              </div>`
+}
